e3d1572c4aba58a91d9e809b0a8f2ad98c6bcfd3
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2022-07-07 10:05:45 +0700

    Login fixed
    swap withcredentials to false

---
+++ src/Page/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import "./assets/style/landing-page.scss"
+
+export default function LandingPage() {
+    return (
+        <div className="p-10 mx-auto bg-neutral-600 ">
+            <p className="para"> NUR FUAD AZIZI </p>
+        </div>
+    )
+}
