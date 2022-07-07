ae36554e24f62847316d3bd2804f5b7ad3df911a
fuadazizi <60860010+fuadazizi@users.noreply.github.com>
2022-07-07 10:06:02 +0700

    Revert "Login fixed"
    This reverts commit e3d1572c4aba58a91d9e809b0a8f2ad98c6bcfd3.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
 
-import LandingPage from './Page/LandingPage'
-
-export default function App() {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-            </Routes>
-        </Router>
-    )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
